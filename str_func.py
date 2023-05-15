10252ea5723f138a3b5259a6aef39946969d2f3e
sergei valdemarov <valdemarovsergei@gmail.com>
2023-05-15 20:34:49 +0300

    Added title_string1

--- str_func.py
+++ str_func.py
@@ -1,6 +1,6 @@
 def upper_string(string):
     return string.upper()
 
-def title_string(string):
+def title_string():
     """Функция делает первый символ каждого слова в строке заглавным"""
     return string.title()
