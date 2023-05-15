6e00bf2413c09bc8fa4c137bfecb6f8d4be42cee
ArchiVald0897 <129623067+ArchiVald0897@users.noreply.github.com>
2023-05-15 20:37:48 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -2,6 +2,4 @@ def upper_string(string):
     """Функция делает все буквы заглавными"""
     return string.upper()
 
-def title_string(string):
-    """Функция делает первый символ каждого слова в строке заглавным"""
-    return string.title()
+
